ca378240c3d5cc395ea08783e6f3fc51aaafcbe6
VISITANTE7@asav.brm <VISITANTE7@asav.brm>
2019-12-18 08:13:26 -0300

    Initial commit

---
+++ app/src/main/java/com/example/layout10/Cadastro.java
@@ -0,0 +1,15 @@
+package com.example.layout10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+
+}
